135134ac3865a31c5d9a5b6b20bd3fcaa610906d
Maxim Liplyanin <liplyanin@yandex.ru>
2020-03-02 19:57:44 +0300

    add solution

--- src/index.js
+++ src/index.js
@@ -38,8 +38,34 @@ const MORSE_TABLE = {
 };
 
 function decode(expr) {
-    // write your solution here
+
+ let arr =[];
+ for(let i = 0; i<expr.length; i+=10){
+        arr.push(expr.slice(i, i+10))
+    }
+    let newArr = [];
+    arr.forEach(function(el) {
+        if(el=='**********') newArr.push(" "); else{
+        let str ='';
+        for(let i=0; i<=el.length; i+=2){
+            if(el.slice(i,i+2)=="10"){
+                str+='.';
+            }else if(el.slice(i,i+2)=="11"){
+                str+="-";
+            }
+        }
+        newArr.push(str);}
+    });
+    let decodeArr = [];
+    newArr.forEach(function(el){
+        if(el==" ")decodeArr.push(" ") ;
+        else   decodeArr.push(MORSE_TABLE[el]);
+    })
+        return decodeArr.join("")
+
+
 }
+
 
 module.exports = {
     decode
